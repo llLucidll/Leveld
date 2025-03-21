86df04ba650fb414880755688f916e3baffcea13
llLucidll <akhil10831@gmail.com>
2025-03-21 13:00:28 -0600

    Added base files

---
+++ Leveld_Back/src/main/java/com/app/Leveld/LeveldApplication.java
@@ -0,0 +1,13 @@
+package com.app.Leveld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeveldApplication.class, args);
+	}
+
+}
---
+++ Leveld_Back/src/test/java/com/app/Leveld/LeveldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Leveld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeveldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
